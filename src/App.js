2d2fd599a777c4a0d9106f775955c553a06586ad
Adrian Menacho <adrianjosemenacho22@gmail.com>
2024-07-17 02:06:53 -0400

    First Commit to Website

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-pink-400 text-2xl'>Welcome to my starter website</h1>
+      <h2 className='text-purple-900 text-'>My name is Adrian Menacho and I'm web ddeveloper in the making</h2>
+      <div className='flex justify-center items-center'>
+        <img className='object-scale-down h-48 w-96' src={'./adrianM.jpg'}/>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
